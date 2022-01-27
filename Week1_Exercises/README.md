8b1c838bce234de665656d474b3dec1cafa670cb
Warren Spalding <w.spalding.toc@gmail.com>
2022-01-27 17:29:26 +0000

    added README.md & exercise1.py

---
+++ Week1_Exercises/README.md
@@ -0,0 +1,4 @@
+# Exercise Criteria
+
+## 1
+Program that asks the user to enter the width and length of a room. Once the values have been read, your program should compute and display the area of the room. The length and the width will be entered as floating point numbers. Includes units in prompt and output message (feet/meters)
---
+++ Week1_Exercises/exercise1.py
@@ -0,0 +1,11 @@
+# Author: Warren Spalding
+# Date: 27 January 2022
+# Python Warm-Up & Recap
+# Exercise 1
+
+room_length = float(input("Please enter length of room in meters: "))
+room_width = float(input("Please enter room width in meters: "))
+
+room_area = room_length * room_width
+
+print("\nYour total room area is", room_area,"meters.")

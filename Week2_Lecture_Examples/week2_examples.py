f10f37ed8e292fd1285a1ccfb1d813948b0e2918
Warren Spalding <w.spalding.toc@gmail.com>
2022-02-08 23:52:46 +0000

    update week2_examples.py

--- Week2_Lecture_Examples/week2_examples.py
+++ Week2_Lecture_Examples/week2_examples.py
@@ -2,36 +2,48 @@
 
 class Car:
     """ A simple Car class example """
+    __doc__ = 'Luxury Car Class'
+
+    """ Class attribute to hold list of Cars created """
     all_cars = []
     
 
     """ Initialising """
     def __init__(self,ma,mo,yr):
-        self.make = ma
-        self.model = mo
-        self.year = yr
+        self._make = ma
+        self._model = mo
+        self._year = yr
         """_odometer_reading should only be
         accessed/modified via internal methods"""
-        self._odometer_reading = 0
+        self.__odometer_reading = 0
         Car.all_cars.append(self)
-    
+
     @classmethod
     def print_inventory(cls):
         for item in cls.all_cars:
             print(item.get_fullname())
-    
+
+    def __str__(self):
+        return 'Car: ' + self._make + ' ' + self._model
+
+    def __len__(self):
+        return self.get_car_age()
+
+    def __eq__(self, other):
+        return (self._make == other._make and self._model == other._model)
+
     def get_fullname(self):
         """ Return a neatly formatted descriptive name """
-        long_name = str(self.year) + ' ' + self.make + \
-            ' ' + self.model
+        long_name = str(self._year) + ' ' + self._make + \
+            ' ' + self._model
         return long_name.title()
 
     def get_car_age(self, current_year = 2022):
         """ Return the age of the car in years """
-        return current_year - self.year
+        return current_year - self._year
 
     def increment_odometer(self, miles):
-        self.odometer_reading += miles
+        self.__odometer_reading += miles
 
 
 #Cars
@@ -42,3 +54,9 @@ au_car_1 = Car('Audi', 'Q7', 2021)
 
 
 Car.print_inventory()
+print(vw_car_1)
+#print(getattr(vw_car_1, '_make'))
+#print(getattr(vw_car_1,'get_fullname'))
+#print(getattr(vw_car_1,'get_fullname')())
+#print(hasattr(vw_car_1,'engine_size'))
+
